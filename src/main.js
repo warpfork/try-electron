10e47d99f05b35035f3a3516b8b84d81e7978748
Eric Myhre <hash@exultant.us>
2025-03-05 03:52:32 +0100

    okay, might've found the secret to limiting remote requests, and fuck yes if so.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require("electron/main");
 
+process.env.ELECTRON_ENABLE_SECURITY_WARNINGS = true;
+
 const createWindow = () => {
   // Each BrowserWindow gets a renderer process, which is sandboxed.
   // Each BrowserWindow also gets a "preload" script, which is... semi sandboxed.
@@ -13,9 +15,13 @@ const createWindow = () => {
   // - call `ipcMain.handle('callname', thunk)` in the main process;
   // - hand a thunk that calls `ipcRenderer.invoke('callname') to a contextBridge in the preload script
   // - and then call that thunk as desired from the renderer side.
+  // See https://www.electronjs.org/docs/latest/tutorial/ipc for more depth.
   //
   // I don't really understand why a preload script seems to have to a literal file,
   // and can't just be a thunk.  I probably don't what to know.  Whatever.
+  // (I guess a considerable part of it is that the require function is Different
+  // in a preload script: and specifically the contextBridge object you can get is now
+  // for *that* window.  So it's an API artifact from an early day, is my guess.)
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -36,3 +42,30 @@ app.whenReady().then(() => {
 
 // I want to know how to allow-list remote content fetching.
 // I want to set the allow-list to *nothing*.
+//
+// From https://www.electronjs.org/docs/latest/tutorial/security :
+// > To display remote content, use the `<webview>` tag
+// > or a `WebContentsView` and make sure to disable the `nodeIntegration`
+// > and enable `contextIsolation`.
+// ... both of which for WebContentsView have been the default for a good while now, happily.
+// (Wtf is contextIsolation?  It's basically "plz no override global vars across renderers".  So... baseline sanity.)
+//
+// OOH, I think this is it: https://www.electronjs.org/docs/latest/tutorial/security#5-handle-session-permission-requests-from-remote-content
+//
+
+// ... but this doesnt quite fly because "Session can only be received when app is ready",
+// and I haven't sussed beyond that yet.
+const { session } = require("electron/main");
+const { URL } = require("url");
+
+session
+  .fromPartition("some-partition") // ???
+  .setPermissionRequestHandler((webContents, permission, callback) => {
+    console.log(
+      "PERMISSIONS REQUEST:",
+      webContents.getURL(),
+      permission,
+      webContents,
+    );
+    return callback(false);
+  });
