8c1aa058053f8c730ac2244e7661b20d5457613d
Eric Myhre <hash@exultant.us>
2025-03-06 22:31:48 +0100

    can we... call require as external?  no.  doesn't change the generated code at all.
    I thought this might be a way forward, because in reviewing
    https://www.electronjs.org/docs/latest/tutorial/sandbox#preload-scripts
    I'm seeing that the require function in this context is very much
    *not the normal one*...
    
    I, uh, I think I might just give up here.
    
    These preload files should be very very short and maybe it's just
    not worth giving a damn about typescript in them at all.

--- src/preload.ts
+++ src/preload.ts
@@ -1,10 +1,10 @@
 // apparently there is not an "electron/preload" type definition subset.  Shame.
-import { contextBridge } from "electron";
+// import { contextBridge } from "electron";
 
 // Okay, and apparently imports don't work in preload scripts, and never will, because...
 // I dunno: https://www.electronjs.org/docs/latest/tutorial/esm#sandboxed-preload-scripts-cant-use-esm-imports
 
-// const { contextBridge } = require("electron");
+const { contextBridge } = require("electron");
 // ^ so this sucks, because then we don't get types in this file.
 // ^ and also doesn't work either, because generates a (different) import statement via bun internals, so does not dtrt.
 // ^ and ALSO somehow the presence of that line in this file causes "electron/main" to stop resolving in the other file,
