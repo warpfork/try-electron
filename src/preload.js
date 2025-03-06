5bb50319a2aa8f87be0d6ea9d7b889d8a661945d
Eric Myhre <hash@exultant.us>
2025-03-06 22:59:27 +0100

    more notes on other roads, but still, meh.  this file is js for now.

--- src/preload.js
+++ src/preload.js
@@ -5,11 +5,25 @@
 // so transpilers tend to do bad things with this file if they touch that function (and they tend to).
 //
 // Also, the use of modules and the import statement is forbidden in preload scripts,
-// which I don't fully understand, but see https://www.electronjs.org/docs/latest/tutorial/esm#sandboxed-preload-scripts-cant-use-esm-imports .
+// which I don't fully understand, but see https://www.electronjs.org/docs/latest/tutorial/esm#sandboxed-preload-scripts-cant-use-esm-imports
+// and also https://www.electronjs.org/docs/latest/tutorial/esm#summary-esm-support-matrix .
+//
+// There may also be options to simply disable sandboxing for preload scripts,
+// mentioned in https://github.com/electron/electron/issues/35587#issuecomment-1247138429 .
+// I don't actually know what negative security implications that might have.
+// (I thought preload scripts are out of scope by the time content is happening?  But I'm unsure.)
+// For now, poking into that seems like cost and risk.
 //
 // So: I give up.  This one just stays as a javascript file.  Moving on.
 
+// To minimize the pain of this file being untyped and being unable to import other files,
+// I think the way forward is to more or less expose one giant object full of RPC powers,
+// and make types for that, and use those types in the main process and in the renderers,
+// and just move on.
+
 const { contextBridge } = require("electron");
+
+// Stuff exposed in the "main world" is also available in the chromium debug console!
 
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
