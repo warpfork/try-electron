3f6bcbc88d05a4a94e0cb875e785ec8b69818b04
Eric Myhre <hash@exultant.us>
2025-03-06 22:19:27 +0100

    so these are not the challenges I was expecting.

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,11 @@
 // apparently there is not an "electron/preload" type definition subset.  Shame.
 import { contextBridge } from "electron";
 
+// const { contextBridge } = require('electron')
+// ^ but this also generates a (different) import statement via bun internals, so does not dtrt.
+// Also somehow the presence of that line in this file causes "electron/main" to stop resolving in the other file,
+//  which is definitely among the more deranged things I've seen today.  Not sure if most deranged.  But up there.
+
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
