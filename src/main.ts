41ec877647ea09150ddcd0496d4717b8c7b78a2e
Eric Myhre <hash@exultant.us>
2025-03-06 22:03:55 +0100

    boy this is just a smorgasboard of shit error handling.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from "electron/main";
+import path from "node:path";
 
 // console.log("hellooo", app, BrowserWindow);
 
@@ -8,7 +9,13 @@ app.whenReady().then(() => {
     height: 600,
     webPreferences: {
       // yes, electron requires preload paths be absolute.  brilliant.  amazing.  reasonable.  (no.)
-      preload: "preload.js",
+      // the above is not a real error that stops anything.  just an "ERROR" in stderr, but doesn't stop anything.
+      // if the file doesn't exist?  that doesn't show up except in the console in-browser -- no sign on stderr.
+      // i feel like i'm being trolled.
+      //
+      // and "__dirname" gets baked by bun as a path in the src tree, which is... not the desired thing here.
+      // so this still doesn't work right, yet.  something else is required.
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
